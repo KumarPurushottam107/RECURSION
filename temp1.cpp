9a6c919a84dc810a507070696ef05bd1ba464d4b
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-02-11 23:53:05 +0530

    Add files via upload

---
+++ temp1.cpp
@@ -0,0 +1,65 @@
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        // code here.
+    if(s.size() == 0)
+        return;
+    int mid = ceil(sizeOfStack/2.0);
+    int val=-1;
+    if(s.size()!=mid)
+        val=s.top();
+    
+    s.pop();
+    
+    deleteMid(s,sizeOfStack);
+        
+        
+    if(val!=-1)
+        s.push(val);
+     
+    return;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+  // } Driver Code Ends
