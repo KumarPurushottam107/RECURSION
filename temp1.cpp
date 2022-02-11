f3d371ade109fad429b9db89d67a854a2c9f03e8
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-02-11 20:02:47 +0530

    Add files via upload

---
+++ temp1.cpp
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+    stack<int> s;
+    void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+        s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+    SortedStack *ss = new SortedStack();
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+    int k;
+    cin>>k;
+    ss->s.push(k);
+    }
+    ss->sort();
+    printStack(ss->s);
+}
+}// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+    stack<int> s;
+    void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+
+void insert1(stack<int>&s,int temp)
+{
+    if(s.size()==0)
+    {
+        s.push(temp);
+        return;
+    }
+    
+    int val = s.top();
+    
+    if(temp>=val)
+    {
+        s.push(temp);
+        return ;
+    }
+    
+    s.pop();
+    
+    insert1(s,temp);
+    
+    s.push(val);
+    
+    return ;
+    
+    
+    
+}
+
+
+void SortedStack :: sort()
+{
+   //Your code here
+  
+  if(s.size()==0)
+    return ;
+    
+    int temp=s.top();
+    
+    s.pop();
+    
+    sort();
+    
+    insert1(s,temp);
+    
+    
+
+    
+   
+   
+}
